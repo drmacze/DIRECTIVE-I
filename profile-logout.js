9bc7b8f6c31ad8e81256449860e16adb2037868b
Drmacze DEV <drma801@gmail.com>
2026-09-10 07:04:45 +0700

    Load official platform branding on homepage

--- profile-logout.js
+++ profile-logout.js
@@ -1,4 +1,12 @@
 (() => {
+  if (!document.querySelector('script[data-directive-official-branding]')) {
+    const branding = document.createElement('script');
+    branding.src = 'official-branding.js?v=1';
+    branding.dataset.directiveOfficialBranding = '';
+    branding.defer = true;
+    document.head.appendChild(branding);
+  }
+
   const PROFILE_KEY = 'directive_minecraft_profile';
   const LOGOUT_REVEAL_KEY = 'directive_logout_reveal';
   const MUSIC_KEYS = [
